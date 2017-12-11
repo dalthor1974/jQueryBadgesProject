cf5ea9d074feacbda692c7b3bcb7fa4a857af537
Dimitrios Danellakis <dalthor1974@yahoo.gr>
2017-12-12 01:34:03 +0200

    First build

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
-
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/dalthor1974.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var courses = response.courses.complete;
+      var el = $('#badges');
+      for (var i = 0; i < courses.length; i++) {
+        var div = $('<div class="course"></div>');
+        el.append(div);
+        div.append('<h3>' + courses[i].name + '</h3>');
+        div.append('<img src="' + courses[i].badge + '" />');
+        div.append('<a href="' + courses[i].url + '" target="_blank" class="btn btn-primary">See Course</a>');
+      }
+    }
+  });
 });
