2ce71abdd10a459bb11fe527236c2edf911d8b6b
Dimitrios Danellakis <dalthor1974@yahoo.gr>
2017-12-12 02:17:06 +0200

    Finished project

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,16 +2,17 @@ $(function() {
   $.ajax({
     url: 'https://www.codeschool.com/users/dalthor1974.json',
     dataType: 'jsonp',
-    success: function(response) {
-      var courses = response.courses.complete;
-      var el = $('#badges');
-      for (var i = 0; i < courses.length; i++) {
-        var div = $('<div />', {class: 'course'});
-        el.append(div);
-        div.append('<h3>' + courses[i].name + '</h3>');
-        div.append('<img src="' + courses[i].badge + '" />');
-        div.append('<a href="' + courses[i].url + '" target="_blank" class="btn btn-primary">See Course</a>');
+    success: function(data) {
+      for (var course of data.courses.completed) {
+        mydiv = $('<div />', {class: 'course'});
+        $('<h3>' + course.title + '</h3>').appendTo(mydiv);
+        $('<img src="' + course.badge + '" />').appendTo(mydiv);
+        $('<a href="' + course.url + '" target="_blank" class="btn btn-primary">See Course</a>').appendTo(mydiv);
+        $('#badges').append(mydiv);
       }
+    },
+    error: function() {
+      alert('AJAX did not work');
     }
   });
 });
